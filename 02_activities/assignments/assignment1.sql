bc79f73d304697ba3b4cb8839c3a474a4261ceb1
LebronBrianSinza <lebronbrian23@gmail.com>
2025-08-10 14:53:16 -0400

    Added new file assignment-one-entity-diagram.png Modified assignment1.sql

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -4,18 +4,22 @@
 
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
-
+SELECT * FROM customer;
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
-
+SELECT * FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
-
+SELECT * FROM customer_purchases
+WHERE product_id = 4
+OR product_id = 9;
 
 -- option 2
 
@@ -27,8 +31,9 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
-
+SELECT *, quantity * cost_to_customer_per_qty as price
+FROM customer_purchases
+WHERE vendor_id BETWEEN 8 AND 10;
 -- option 2
 
 
@@ -38,20 +43,38 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 Using the product table, write a query that outputs the product_id and product_name
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
-
+SELECT product_id,  product_name,
+CASE WHEN product_qty_type = 'unit' 
+THEN 'unit'
+ELSE 'bulk'
+END as prod_qty_type_condensed
+FROM product;
 
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
-
+SELECT product_id,  product_name,
+CASE WHEN product_qty_type = 'unit' 
+THEN 'unit'
+ELSE 'bulk'
+END as prod_qty_type_condensed,
+CASE WHEN product_name like '%pepper%'
+THEN  1
+ELSE 0
+END as pepper_flag 
+FROM product;
 
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
 
-
+SELECT *
+FROM vendor as v
+INNER JOIN vendor_booth_assignments as vba
+ON v.vendor_id = vba.vendor_id
+ORDER BY vendor_name, market_date;
 
 
 /* SECTION 3 */
@@ -59,7 +82,9 @@ vendor_id field they both have in common, and sorts the result by vendor_name, t
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
-
+SELECT vendor_id, count(vendor_id) as booths_rented
+FROM vendor_booth_assignments 
+GROUP BY vendor_id;
 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
@@ -67,7 +92,17 @@ sticker to everyone who has ever spent more than $2000 at the market. Write a qu
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-
+SELECT 
+c.customer_id,
+c.customer_first_name,
+c.customer_last_name,
+ROUND(SUM(cp.quantity * cp.cost_to_customer_per_qty),2) as expenditure
+FROM  customer c
+INNER JOIN customer_purchases cp
+ON c.customer_id = cp.customer_id
+GROUP BY cp.customer_id
+HAVING expenditure > 2000
+ORDER BY c.customer_last_name , c.customer_first_name;
 
 
 --Temp Table
@@ -81,7 +116,18 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+DROP TABLE IF EXISTS temp.new_vendor;
 
+CREATE TABLE temp.new_vendor AS
+SELECT * FROM vendor;
+
+INSERT INTO temp.new_vendor VALUES (
+10,
+'Thomass Superfood Store',
+'Fresh Focused store', 
+'Thomas', 
+'Rosenthal'
+);
 
 
 -- Date
